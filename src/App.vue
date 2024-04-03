ccd7bbd6ac59803f01a5888ec00b50d3be69c024
NotTofumi <dominikmucharzewski@gmail.com>
2024-04-03 19:35:13 +0200

    improved design based on customer feedback

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Home.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+  <body>
+    
+  <Navbar/>
   <HelloWorld/>
+  </body>
 </template>
 
 <style scoped>
+body{
+  background-color: #fdfdfd;
+  background-image: url(./assets/Hintergrund.png.png);
+  background-size: contain;
+  flex-wrap: wrap;
+  background-repeat: no-repeat;
+}
 </style>
