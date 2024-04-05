52b012116e8ea82b30e8dfa4d047ebd8fed8c880
NotTofumi <dominikmucharzewski@gmail.com>
2024-04-05 18:24:21 +0200

    temporarily removed footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/Home.vue'
 import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
 </script>
 
 <template>
@@ -9,7 +8,6 @@ import Footer from './components/Footer.vue'
     
   <Navbar/>
   <HelloWorld/>
-  <Footer/>
   </body>
 </template>
 
