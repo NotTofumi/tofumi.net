230f015ac32278a15a6ff877e2bfe1a402afb3fe
NotTofumi <dominikmucharzewski@gmail.com>
2024-10-12 18:12:20 +0200

    removed stuff

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,6 @@ import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import Home from './components/Home.vue'
-import Betreuung from './components/Fortbildungen/Betreuung.vue'
-import Pflege from './components/Fortbildungen/Pflege.vue'
-import PraxisAnleitung from './components/Fortbildungen/PraxisAnleitung.vue'
-import Fuehrung from './components/Unsere Kurse/Fuehrung.vue'
-import Pflegende from './components/Unsere Kurse/Pflegende.vue'
-import Studierende from './components/Unsere Kurse/Studierende.vue'
-import Betreungskraft from './components/Weiterbildungen/Betreungskraft.vue'
-import Pflegehelfer from './components/Weiterbildungen/Pflegehelfer.vue'
-import Arbeitsorganisation from './components/Weitere Leistungen/Arbeitsorganisation.vue'
-import Gesundheitsmanagement from './components/Weitere Leistungen/Gesundheitsmanagement.vue'
 
 const routes = [
 { path: "/", component: Home},
